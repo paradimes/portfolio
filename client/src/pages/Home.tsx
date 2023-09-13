c59d968d25aea081929c03ef626bcdb34ee2380f
AM <test@test.com>
2023-09-12 21:19:15 -0400

    dark mode system settings support

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,26 @@ import Navbar from "../components/Navbar";
 import Content from "../components/Content";
 
 export default function Home() {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  // Whenever the user explicitly chooses light mode
+  localStorage.theme = "light";
+
+  // Whenever the user explicitly chooses dark mode
+  localStorage.theme = "dark";
+
+  // Whenever the user explicitly chooses to respect the OS preference
+  localStorage.removeItem("theme");
+
   return (
     <Flex direction="column" className="h-screen bg-white dark:bg-slate-900">
       <Navbar />
